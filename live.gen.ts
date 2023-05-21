6a9b52ac961203933e56371aa88299a002265ed7
Kleber <kleberst88@gmail.com>
2023-05-21 11:00:24 -0300

    feat: Adicionado os componentes Logos, Testimonial e ImageGallery
    componentes importados das PRs do hackthon

--- live.gen.ts
+++ live.gen.ts
@@ -4,34 +4,37 @@
 
 import config from "./deno.json" assert { type: "json" };
 import { DecoManifest } from "$live/types.ts";
-import * as $$$$0 from "./routes/styles.css.ts";
-import * as $$$$1 from "./routes/_app.tsx";
+import * as $$$$0 from "./routes/_app.tsx";
+import * as $$$$1 from "./routes/styles.css.ts";
 import * as $$$$$0 from "./islands/Newsletter.tsx";
-import * as $$$$$1 from "./islands/WishlistButton.tsx";
-import * as $$$$$2 from "./islands/ShippingSimulation.tsx";
-import * as $$$$$3 from "./islands/HeaderSearchbar.tsx";
-import * as $$$$$4 from "./islands/HeaderButton.tsx";
-import * as $$$$$5 from "./islands/AddToCartButton.tsx";
-import * as $$$$$6 from "./islands/HeaderModals.tsx";
-import * as $$$$$7 from "./islands/SliderJS.tsx";
-import * as $$$$$8 from "./islands/ProductImageZoom.tsx";
-import * as $$$$$9 from "./islands/OutOfStock.tsx";
-import * as $$$$$10 from "./islands/SearchControls.tsx";
-import * as $$$$$$$$0 from "./sections/WishlistGallery.tsx";
-import * as $$$$$$$$1 from "./sections/LinkTree.tsx";
-import * as $$$$$$$$2 from "./sections/Features.tsx";
-import * as $$$$$$$$3 from "./sections/BannerPLP.tsx";
-import * as $$$$$$$$4 from "./sections/BannerGrid.tsx";
-import * as $$$$$$$$5 from "./sections/SearchResult.tsx";
-import * as $$$$$$$$6 from "./sections/ProductShelf.tsx";
-import * as $$$$$$$$7 from "./sections/Footer.tsx";
-import * as $$$$$$$$8 from "./sections/CookieConsent.tsx";
-import * as $$$$$$$$9 from "./sections/DesignSystem.tsx";
-import * as $$$$$$$$10 from "./sections/Header.tsx";
-import * as $$$$$$$$11 from "./sections/ProductDetails.tsx";
-import * as $$$$$$$$12 from "./sections/Highlights.tsx";
-import * as $$$$$$$$13 from "./sections/WhatsApp.tsx";
-import * as $$$$$$$$14 from "./sections/Carousel.tsx";
+import * as $$$$$1 from "./islands/ShippingSimulation.tsx";
+import * as $$$$$2 from "./islands/OutOfStock.tsx";
+import * as $$$$$3 from "./islands/AddToCartButton.tsx";
+import * as $$$$$4 from "./islands/ProductImageZoom.tsx";
+import * as $$$$$5 from "./islands/HeaderSearchbar.tsx";
+import * as $$$$$6 from "./islands/SliderJS.tsx";
+import * as $$$$$7 from "./islands/HeaderModals.tsx";
+import * as $$$$$8 from "./islands/HeaderButton.tsx";
+import * as $$$$$9 from "./islands/SearchControls.tsx";
+import * as $$$$$10 from "./islands/WishlistButton.tsx";
+import * as $$$$$$$$0 from "./sections/Carousel.tsx";
+import * as $$$$$$$$1 from "./sections/Features.tsx";
+import * as $$$$$$$$2 from "./sections/Logos/Logos.tsx";
+import * as $$$$$$$$3 from "./sections/ProductDetails.tsx";
+import * as $$$$$$$$4 from "./sections/CookieConsent.tsx";
+import * as $$$$$$$$5 from "./sections/Footer.tsx";
+import * as $$$$$$$$6 from "./sections/Header.tsx";
+import * as $$$$$$$$7 from "./sections/ImageGallery/ImageGallery.tsx";
+import * as $$$$$$$$8 from "./sections/BannerGrid.tsx";
+import * as $$$$$$$$9 from "./sections/WhatsApp.tsx";
+import * as $$$$$$$$10 from "./sections/ProductShelf.tsx";
+import * as $$$$$$$$11 from "./sections/WishlistGallery.tsx";
+import * as $$$$$$$$12 from "./sections/BannerPLP.tsx";
+import * as $$$$$$$$13 from "./sections/DesignSystem.tsx";
+import * as $$$$$$$$14 from "./sections/Testimonial/Testimonial.tsx";
+import * as $$$$$$$$15 from "./sections/Highlights.tsx";
+import * as $$$$$$$$16 from "./sections/LinkTree.tsx";
+import * as $$$$$$$$17 from "./sections/SearchResult.tsx";
 import * as $live_middleware from "$live/routes/_middleware.ts";
 import * as $live_workbench from "$live/routes/live/workbench.ts";
 import * as $live_invoke from "$live/routes/live/invoke/index.ts";
@@ -141,7 +144,7 @@ import * as i2$$$$$19 from "deco-sites/std/actions/vtex/wishlist/removeItem.ts";
 
 const manifest = {
   "routes": {
-    "./routes/_app.tsx": $$$$1,
+    "./routes/_app.tsx": $$$$0,
     "./routes/_middleware.ts": $live_middleware,
     "./routes/[...catchall].tsx": $live_catchall,
     "./routes/index.tsx": $live_catchall,
@@ -152,40 +155,43 @@ const manifest = {
     "./routes/live/invoke/index.ts": $live_invoke,
     "./routes/live/previews/[...block].tsx": $live_previews,
     "./routes/live/workbench.ts": $live_workbench,
-    "./routes/styles.css.ts": $$$$0,
+    "./routes/styles.css.ts": $$$$1,
   },
   "islands": {
-    "./islands/AddToCartButton.tsx": $$$$$5,
-    "./islands/HeaderButton.tsx": $$$$$4,
-    "./islands/HeaderModals.tsx": $$$$$6,
-    "./islands/HeaderSearchbar.tsx": $$$$$3,
+    "./islands/AddToCartButton.tsx": $$$$$3,
+    "./islands/HeaderButton.tsx": $$$$$8,
+    "./islands/HeaderModals.tsx": $$$$$7,
+    "./islands/HeaderSearchbar.tsx": $$$$$5,
     "./islands/Newsletter.tsx": $$$$$0,
-    "./islands/OutOfStock.tsx": $$$$$9,
-    "./islands/ProductImageZoom.tsx": $$$$$8,
-    "./islands/SearchControls.tsx": $$$$$10,
-    "./islands/ShippingSimulation.tsx": $$$$$2,
-    "./islands/SliderJS.tsx": $$$$$7,
-    "./islands/WishlistButton.tsx": $$$$$1,
+    "./islands/OutOfStock.tsx": $$$$$2,
+    "./islands/ProductImageZoom.tsx": $$$$$4,
+    "./islands/SearchControls.tsx": $$$$$9,
+    "./islands/ShippingSimulation.tsx": $$$$$1,
+    "./islands/SliderJS.tsx": $$$$$6,
+    "./islands/WishlistButton.tsx": $$$$$10,
   },
   "sections": {
     "$live/sections/PageInclude.tsx": i2$$$$0,
     "$live/sections/Slot.tsx": i2$$$$1,
     "$live/sections/UseSlot.tsx": i2$$$$2,
-    "deco-sites/107store/sections/BannerGrid.tsx": $$$$$$$$4,
-    "deco-sites/107store/sections/BannerPLP.tsx": $$$$$$$$3,
-    "deco-sites/107store/sections/Carousel.tsx": $$$$$$$$14,
-    "deco-sites/107store/sections/CookieConsent.tsx": $$$$$$$$8,
-    "deco-sites/107store/sections/DesignSystem.tsx": $$$$$$$$9,
-    "deco-sites/107store/sections/Features.tsx": $$$$$$$$2,
-    "deco-sites/107store/sections/Footer.tsx": $$$$$$$$7,
-    "deco-sites/107store/sections/Header.tsx": $$$$$$$$10,
-    "deco-sites/107store/sections/Highlights.tsx": $$$$$$$$12,
-    "deco-sites/107store/sections/LinkTree.tsx": $$$$$$$$1,
-    "deco-sites/107store/sections/ProductDetails.tsx": $$$$$$$$11,
-    "deco-sites/107store/sections/ProductShelf.tsx": $$$$$$$$6,
-    "deco-sites/107store/sections/SearchResult.tsx": $$$$$$$$5,
-    "deco-sites/107store/sections/WhatsApp.tsx": $$$$$$$$13,
-    "deco-sites/107store/sections/WishlistGallery.tsx": $$$$$$$$0,
+    "deco-sites/107store/sections/BannerGrid.tsx": $$$$$$$$8,
+    "deco-sites/107store/sections/BannerPLP.tsx": $$$$$$$$12,
+    "deco-sites/107store/sections/Carousel.tsx": $$$$$$$$0,
+    "deco-sites/107store/sections/CookieConsent.tsx": $$$$$$$$4,
+    "deco-sites/107store/sections/DesignSystem.tsx": $$$$$$$$13,
+    "deco-sites/107store/sections/Features.tsx": $$$$$$$$1,
+    "deco-sites/107store/sections/Footer.tsx": $$$$$$$$5,
+    "deco-sites/107store/sections/Header.tsx": $$$$$$$$6,
+    "deco-sites/107store/sections/Highlights.tsx": $$$$$$$$15,
+    "deco-sites/107store/sections/ImageGallery/ImageGallery.tsx": $$$$$$$$7,
+    "deco-sites/107store/sections/LinkTree.tsx": $$$$$$$$16,
+    "deco-sites/107store/sections/Logos/Logos.tsx": $$$$$$$$2,
+    "deco-sites/107store/sections/ProductDetails.tsx": $$$$$$$$3,
+    "deco-sites/107store/sections/ProductShelf.tsx": $$$$$$$$10,
+    "deco-sites/107store/sections/SearchResult.tsx": $$$$$$$$17,
+    "deco-sites/107store/sections/Testimonial/Testimonial.tsx": $$$$$$$$14,
+    "deco-sites/107store/sections/WhatsApp.tsx": $$$$$$$$9,
+    "deco-sites/107store/sections/WishlistGallery.tsx": $$$$$$$$11,
     "deco-sites/std/sections/Analytics.tsx": i2$$$$3,
     "deco-sites/std/sections/configButterCMS.global.tsx": i2$$$$4,
     "deco-sites/std/sections/configOCC.global.tsx": i2$$$$5,
---
+++ sections/Testimonial/Testimonial.tsx
@@ -0,0 +1,64 @@
+import type { Image as ImageType } from "deco-sites/std/components/types.ts";
+import Image from "deco-sites/std/components/Image.tsx";
+
+export interface Props {
+  title: string;
+  subtitle: string;
+  testimonials: Array<{
+    /** @description Company Logo */
+    logo: ImageType;
+    description: string;
+    /** @description Profile Logo */
+    image: ImageType;
+    name: string;
+    /** @description Position in company */
+    position: string;
+  }>;
+}
+
+export default function Testimonials({
+  title,
+  subtitle,
+  testimonials,
+}: Props) {
+  return (
+    <div className="w-full mx-4">
+      <div className="flex flex-col w-full m-4">
+        <div className="w-full text-center py-8">
+          <h1 className="text-4xl">{title}</h1>
+          <h3 className="text-lg">{subtitle}</h3>
+        </div>
+
+        <div className="flex flex-wrap justify-center">
+          {testimonials.map((testimonial) => (
+            <div className="w-full py-4 lg:w-1/3">
+              <div className="flex flex-col items-center">
+                <div className="avatar">
+                  <div className="w-16 rounded-xl">
+                    <img src={testimonial.logo} />
+                  </div>
+                </div>
+              </div>
+
+              <div className="text-center w-2/3 mx-auto py-4">
+                <p className="text-sm">{testimonial.description}</p>
+              </div>
+
+              <div className="flex flex-col items-center">
+                <div className="avatar">
+                  <div className="w-12 rounded-xl">
+                    <img src={testimonial.image} />
+                  </div>
+                </div>
+                <div className="text-center">
+                  <h3 className="text-lg">{testimonial.name}</h3>
+                  <h4 className="text-sm">{testimonial.position}</h4>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
